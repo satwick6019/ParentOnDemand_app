5494b66f1a9dcdedaf8faed6eed937916df8e6e1
satwick <singhsatwick0@gmail.com>
2026-03-26 01:36:08 +0530

    fixed url problem no3

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('', include('users.urls')),
     path('',include('dashboard.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
